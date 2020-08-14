7a782dc31a959003cbf7ef07a8073b315ece0f01
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2020-08-14 15:34:12 +0200

    fix detailes

--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import map from "../../assets/map.png";
-import busColor from '../../assets/bus-color.png'
+import busColor from "../../assets/bus-color.png";
 
 const Information = () => {
   return (
@@ -9,39 +9,45 @@ const Information = () => {
         <h1 className="information-title-section">EL TALLER.</h1>
         <div className="map">
           <img src={map} alt="map" className="map-img" />
-          <p className="address">Dirección</p>
-          <p className="address-description">
-            Olazabal 169, entre Uriarte y Antonio Saenz, Boulogne Sur Mer (Zona
-            Norte).
-          </p>
-          <h3 className="groups">Grupos reducidos a convenir</h3>
-          <h3 className="transport">Medios de transporte</h3>
-          <div className="bus">
-            <div className="bus-description">
-              <p className="bus-description-text">
-                <strong>314</strong>
+          <div className="left-right-container">
+            <div className="left">
+              <p className="address">Dirección</p>
+              <p className="address-description">
+                Olazabal 169, entre Uriarte y Antonio Saenz, Boulogne Sur Mer
+                (Zona Norte).
               </p>
-              <img src={busColor} alt="" className="image-bus-color"/>
+              <h3 className="groups">Grupos reducidos a convenir</h3>
             </div>
-            <div className="bus-description">
-              <p className="bus-description-text">
-                <strong>707</strong>
-              </p>
-            </div>
-            <div className="bus-description">
-              <p className="bus-description-text">
-                <strong>138</strong> (Costera)
-              </p>
-            </div>
-            <div className="bus-description">
-              <p className="bus-description-text">
-                <strong>437</strong>
-              </p>
-            </div>
-            <div className="bus-description">
-              <p className="bus-description-text">
-                <strong>130</strong> (Estación Boulogne Sur Mer)
-              </p>
+            <div className="right">
+              <h3 className="transport">Medios de transporte</h3>
+              <div className="bus">
+                <div className="bus-description">
+                  <p className="bus-description-text">
+                    <strong>314</strong>
+                  </p>
+                  <img src={busColor} alt="" className="image-bus-color" />
+                </div>
+                <div className="bus-description">
+                  <p className="bus-description-text">
+                    <strong>707</strong>
+                  </p>
+                </div>
+                <div className="bus-description">
+                  <p className="bus-description-text">
+                    <strong>138</strong> (Costera)
+                  </p>
+                </div>
+                <div className="bus-description">
+                  <p className="bus-description-text">
+                    <strong>437</strong>
+                  </p>
+                </div>
+                <div className="bus-description">
+                  <p className="bus-description-text">
+                    <strong>130</strong> (Estación Boulogne Sur Mer)
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
