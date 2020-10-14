572a04149ea0a8953d5cc30f2bedfc76ec62c7ac
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2020-10-14 16:22:33 +0200

    added more images and fix gallery component

---
+++ src/data.js
@@ -0,0 +1,117 @@
+import image1 from './assets/gallery/1.png';
+import image2 from './assets/gallery/2.png';
+import image3 from './assets/gallery/3.png';
+import image4 from './assets/gallery/4.png';
+import image5 from './assets/gallery/5.png';
+import image6 from './assets/gallery/6.png';
+import image7 from './assets/gallery/7.png';
+import image8 from './assets/gallery/8.png';
+import image9 from './assets/gallery/9.png';
+import image10 from './assets/gallery/10.png';
+import image11 from './assets/gallery/11.png';
+import image12 from './assets/gallery/12.png';
+import image13 from './assets/gallery/13.png';
+import image14 from './assets/gallery/14.png';
+import image15 from './assets/gallery/15.png';
+import image16 from './assets/gallery/16.png';
+import image17 from './assets/gallery/17.png';
+import image18 from './assets/gallery/18.png';
+import image19 from './assets/gallery/19.png';
+import image20 from './assets/gallery/20.png';
+import image21 from './assets/gallery/21.png';
+import image22 from './assets/gallery/22.png';
+import image23 from './assets/gallery/23.png';
+import image24 from './assets/gallery/24.png';
+import image25 from './assets/gallery/25.png';
+import image26 from './assets/gallery/26.png';
+import image27 from './assets/gallery/27.png';
+import image28 from './assets/gallery/28.png';
+
+
+export const data = [
+  { 
+    src: image1,
+  },
+  { 
+    src: image2,
+  },
+  { 
+    src: image3,
+  },
+  { 
+    src: image4,
+  },
+  { 
+    src: image5,
+  },
+  { 
+    src: image6,
+  },
+  { 
+    src: image7,
+  },
+  { 
+    src: image8,
+  },
+  { 
+    src: image9,
+  },
+  { 
+    src: image10,
+  },
+  { 
+    src: image11,
+  },
+  { 
+    src: image12,
+  },
+  { 
+    src: image13,
+  },
+  { 
+    src: image14,
+  },
+  { 
+    src: image15,
+  },
+  { 
+    src: image16,
+  },
+  { 
+    src: image17,
+  },
+  { 
+    src: image18,
+  },
+  { 
+    src: image19,
+  },
+  { 
+    src: image20,
+  },
+  { 
+    src: image21,
+  },
+  { 
+    src: image22,
+  },
+  { 
+    src: image23,
+  },
+  { 
+    src: image24,
+  },
+  { 
+    src: image25,
+  },
+  { 
+    src: image26,
+  },
+  { 
+    src: image27,
+  },
+  { 
+    src: image28,
+  },
+
+]
--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -1,44 +1,12 @@
 import React from "react";
-import image1 from '../../assets/gallery/1.png';
-import image2 from '../../assets/gallery/2.png';
-import image3 from '../../assets/gallery/3.png';
-import image4 from '../../assets/gallery/4.png';
-import image5 from '../../assets/gallery/5.png';
-import image6 from '../../assets/gallery/6.png';
-import image7 from '../../assets/gallery/7.png';
-import image8 from '../../assets/gallery/8.png';
-import image9 from '../../assets/gallery/9.png';
-import image10 from '../../assets/gallery/10.png';
-import image11 from '../../assets/gallery/11.png';
-import image12 from '../../assets/gallery/12.png';
-import image13 from '../../assets/gallery/13.png';
-import image14 from '../../assets/gallery/14.png';
-import image15 from '../../assets/gallery/15.png';
-import image16 from '../../assets/gallery/16.png';
-import image17 from '../../assets/gallery/17.png';
+import { data } from '../../data';
 
 const Gallery = () => {
   return (
     <>
       <div role="main" className="main-container-gallery">
         <div className="side-gallery">
-          <img className="image-gallery" src={image1} alt="" />
-          <img className="image-gallery" src={image2} alt="" />
-          <img className="image-gallery" src={image3} alt="" />
-          <img className="image-gallery" src={image4} alt="" />
-          <img className="image-gallery" src={image14} alt="" />
-          <img className="image-gallery" src={image5} alt="" />
-          <img className="image-gallery" src={image6} alt="" />
-          <img className="image-gallery" src={image7} alt="" />
-          <img className="image-gallery" src={image8} alt="" />
-          <img className="image-gallery" src={image9} alt="" />
-          <img className="image-gallery" src={image16} alt="" />
-          <img className="image-gallery" src={image17} alt="" />
-          <img className="image-gallery" src={image10} alt="" />
-          <img className="image-gallery" src={image11} alt="" />
-          <img className="image-gallery" src={image12} alt="" />
-          <img className="image-gallery" src={image13} alt="" />
-          <img className="image-gallery" src={image15} alt="" />
+          { data.map((el) => <img className="image-gallery" src={el.src} alt="" />) }
         </div>
       </div>
     </>
